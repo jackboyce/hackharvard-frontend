d6bdeb689904b7f1cdb94bc7d8d11fcae9d905ad
Jack boyce <boyce.j3131@gmail.com>
2017-10-21 03:04:36 -0400

    Added example map marker

--- app/src/main/java/com/squeegee/ruffinit/MapActivity.kt
+++ app/src/main/java/com/squeegee/ruffinit/MapActivity.kt
@@ -3,6 +3,8 @@ package com.squeegee.ruffinit
 import android.view.View
 import android.view.ViewManager
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.jetbrains.anko.verticalLayout
 
 
@@ -16,7 +18,10 @@ class MapActivity: BaseActivity() {
                 .add(id, mapFrag)
                 .commit()
 
-            mapFrag.getMapAsync {  }
+            mapFrag.getMapAsync {
+                println("fin")
+                it.addMarker(MarkerOptions().position(LatLng(10.0, 10.0)).title("test"))
+            }
         }
     }
 
